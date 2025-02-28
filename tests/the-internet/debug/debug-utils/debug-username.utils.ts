a8d5f7fd06097e4759b99cc56ea435ba61c95e10
Lukasz QATesterGM <gajdalukasz89@gmail.com>
2025-02-28 13:40:29 +0100

    debug methods

---
+++ tests/the-internet/debug/debug-utils/debug-username.utils.ts
@@ -0,0 +1,7 @@
+export function nameToLowerCase(username: string): string {
+  return username.toLowerCase();
+  
+  //bug???
+  // if (username.length < 15) return username.toLowerCase();
+  // return 'tomsmith';
+}
