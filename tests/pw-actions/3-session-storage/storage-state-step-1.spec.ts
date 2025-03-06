865d826eb967c878a6b9d74842d67ee37aee17dd
Lukasz QATesterGM <gajdalukasz89@gmail.com>
2025-03-06 11:27:38 +0100

    storage state part 1

---
+++ tests/pw-actions/3-session-storage/storage-state-step-1.spec.ts
@@ -0,0 +1,46 @@
+import { expect, test } from '@playwright/test';
+import { ADMIN_LOGIN_SESSION, USER_LOGIN_SESSION } from '../../../playwright.config';
+
+test.describe('Advanced examples', () => {
+  test('save storage state - admin', { tag: '@session' }, async ({ page }) => {
+    await page.goto('https://pwts.dev/examples/ui/session/index.php');
+
+    await page.fill('#username', 'admin');
+    await page.fill('#password', 'admin123');
+    await page.click('button');
+
+    await page.context().storageState({ path: ADMIN_LOGIN_SESSION });
+
+    await expect(page.getByText('Jesteś zalogowany jako admin')).toBeVisible();
+  });
+
+  test('load storage state - admin', { tag: '@session' }, async ({ browser }) => {
+    const context = await browser.newContext({ storageState: ADMIN_LOGIN_SESSION });
+    const page = await context.newPage();
+
+    await page.goto('https://pwts.dev/examples/ui/session/books.php');
+
+    await expect(page.getByText('Jesteś zalogowany jako admin')).toBeVisible();
+  });
+
+  test('save storage state - user', { tag: '@session' }, async ({ page }) => {
+    await page.goto('https://pwts.dev/examples/ui/session/index.php');
+
+    await page.fill('#username', 'user');
+    await page.fill('#password', 'user123');
+    await page.click('button');
+
+    await page.context().storageState({ path: USER_LOGIN_SESSION });
+
+    await expect(page.getByText('Jesteś zalogowany jako użytkownik')).toBeVisible();
+  });
+
+  test('load storage state - user', { tag: '@session' }, async ({ browser }) => {
+    const context = await browser.newContext({ storageState: USER_LOGIN_SESSION });
+    const page = await context.newPage();
+
+    await page.goto('https://pwts.dev/examples/ui/session/books.php');
+
+    await expect(page.getByText('Jesteś zalogowany jako użytkownik')).toBeVisible();
+  });
+});
